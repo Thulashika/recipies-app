95d19ee33f924713913c26adab3990330bdfc670
Thulashika <lasibala24@gmail.com>
2025-02-19 00:13:32 +0530

    session 23 changes

--- recipe_app/lib/screens/home_screen.dart
+++ recipe_app/lib/screens/home_screen.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
 import 'package:recipe_app/components/category_grid_item.dart';
 import 'package:recipe_app/components/custom_grid.dart';
 import 'package:recipe_app/data/sample_categories.dart';
 import 'package:recipe_app/routes/router_functions.dart';
+
+import 'package:recipe_app/components/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,26 +15,56 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const CustomDrawer(),
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Recipe App',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w900,
-                color: Colors.grey.shade800,
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Text(
+          //     'Recipe App',
+          //     style: TextStyle(
+          //       fontSize: 36,
+          //       fontWeight: FontWeight.w900,
+          //       color: Colors.grey.shade800,
                 
-              ),
-              // style: GoogleFonts.inter(),
-              ),
-          ),
+          //     ),
+          //     // style: GoogleFonts.inter(),
+          //     ),
+          // ),
+
+           Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Recipe App',
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.grey.shade800,
+                    ),
+                    // style: GoogleFonts.lato(),
+                  ),
+                ),
+          
       
-            // const Gap(16),
+            const Gap(8),
+            Builder(
+                  builder: (context) {
+                    return IconButton(
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      icon: const Icon(Icons.menu),
+                    );
+                  },
+                ),
+              ],
+           ),
       
             // ...foodCategories.map((Category) => Text(Category)),
       
